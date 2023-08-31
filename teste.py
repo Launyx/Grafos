b3b28eaedcb004be673696c369e5c75192d301d4
Luan <109304043+Launyx@users.noreply.github.com>
2023-08-31 20:33:01 -0300

    Mostrando caminho entre dois vértices

--- teste.py
+++ teste.py
@@ -6,19 +6,34 @@ matriz = [[2,1,0,1,0],
           [0,1,0,2,0],
           [1,1,2,0,1],
           [0,0,0,1,0]]
+
+
+caminho = []
 ordem = (len(V))
 grau = []   
 tamanho = 0
 print("A ordem do Grafo é", ordem) # Ordem é o número de vértices
 
-vertice1 = V.index(input("Digite o primeiro vértice: ").upper())
-vertice2 = V.index(input("Digite o segundo vértice: ").upper())
+correcao = False
 
-for i in range(ordem):  #percorre a linha da matriz
-    grau.append(0)  # Adiciona um espaço novo no vetor 
-    for j in range(ordem):  # percorre a coluna da matriz
+while correcao == False:
+    vertice1 = V.index(input("Digite o primeiro vértice: ").upper())    # Obtém um vértice do usuário e coloca em maiúsculo 
+    vertice2 = V.index(input("Digite o segundo vértice: ").upper())     # Com esse resultado, pega o indice em que esse vértice está no vetor V[] e o aloca em uma variável
+
+    if vertice1 != vertice2:
+        correcao = True
+    else:
+        print("Erro, digite vértices diferentes")
+
+
+caminho.append(V[vertice1]) # Coloca o primeiro vértice do caminho da lista caminho
+
+for i in range(ordem):  #percorre as linhas da matriz
+    grau.append(0)  # Adiciona um espaço novo no vetor grau 
+    for j in range(ordem):  # percorre as colunas da matriz
         grau[i] = grau[i] + matriz[i][j]    # soma o valor da matriz no local atual ao vetor atual que está com o valor 0
         tamanho = tamanho + matriz[i][j]    # Soma os valores de cada local da matriz
+
     print("Grau do vetor", V[i],"é" , grau[i]) #Grau é a somatória de todos os números de um vértice
 print("O tamanho do grafo é", tamanho/2) # tamanho é a soma de todos os números de uma matriz divido por 2
 
@@ -34,7 +49,19 @@ for i in range(ordem):
 print("O vertice com menor Grau é ", V[menor])
 print("O vertice com maior Grau é ", V[maior])
 
-if matriz[vertice1][vertice2] > 0:
+for i in range(ordem):
+    if i > vertice1:    # verifica os valores das colunas depois do primeiro vértice
+        if matriz[vertice1][i] > 0: # verifica se os valores das colunas da linha do primeiro vértice são maiores que 0 (tem conexão)
+            caminho.append(V[i])    # coloca a letra na lista caminho, baseada na coluna do primeiro vértice
+
+caminho.append(V[vertice2]) # coloca o último vértice do caminho no fim da lista caminho
+
+tamanhoCaminho = len(caminho)
+
+if matriz[vertice1][vertice2] > 0:  # Verifica se os vértices dados pelo usuário são vizinhos
     print(V[vertice1],"e" , V[vertice2], "são vizinhos")
 else:
     print(V[vertice1],"e" , V[vertice2], "não são vizinhos")
+print("Caminho de", V[vertice1], "até", V[vertice2], ": ", end="")
+for i in range(tamanhoCaminho):
+    print(caminho[i], end=' ')
